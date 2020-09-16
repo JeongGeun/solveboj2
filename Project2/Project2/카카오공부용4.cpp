7e3f05c562e3fd904ea7d15c62aeeb5ac292e1b9
JeongGeun <geun5310@naver.com>
2020-09-16 23:12:40 +0900

    플로이드워셜

--- Project2/Project2/카카오공부용4.cpp
+++ Project2/Project2/카카오공부용4.cpp
@@ -2,6 +2,15 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+
+/***
+    플로이드 워셜 응용문제 
+    
+    1. 모든 노드의 dist를 무한대로 세팅한다.
+    2. 그래프를 그린다.
+    3. 플로이드 워셜 알고리즘을 이용해 각 노드마다의 최단 거리를 계산한다.
+    4. 1~N 노드 중 어디서 내릴지 모르기 때문에 1부터 N까지 내리는 점으로 지정하여 (start에서 i)+(a에서 i까지 거리)+(b에서 i까지 거리)<answer이면 answer갱신 
+***/
 int graph[201][201];
 int visit[201][201];
 
@@ -11,6 +20,7 @@ int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
     {
         for (int j = 0; j < 201; j++)
         {
+            if(i!=j)
             graph[i][j] = 1e9;
         }
     }
@@ -33,18 +43,14 @@ int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
     }
     int sa = graph[s][a];
     int sb = graph[s][b];
+    answer = sa+sb;
     for (int i = 1; i <= n; i++)
     {
-        if (graph[s][i] + graph[i][a] < sa)
+        if (graph[s][i] + graph[i][a]+ graph[i][b] < answer)
         {
-            sa = graph[s][i] + graph[i][a];
-        }
-
-        if (graph[s][i] + graph[i][b] < sb)
-        {
-            sb = graph[s][i] + graph[i][b];
+            answer = graph[s][i] + graph[i][a] + graph[i][b];
         }
     }
-    answer = graph[s][a] + graph[s][b];
+    
     return answer;
 }
